8151d985e25c2c53b71d79bad4127fc214274c8f
David Shin <davidshin444@gmail.com>
2016-12-17 15:27:48 -0800

    Saving current progress

--- seeds/4_prices.js
+++ seeds/4_prices.js
@@ -3,5 +3,5 @@ let seeder = require('knex-csv-seeder').seeder;
 console.log("seeder", seeder);
 exports.seed = seeder.seed({
     table: 'prices',
-    file: '/Users/dave/Galvanize/homework/q2/frugi/models/user_products_seeds.csv'
+    file: '/Users/dave/Galvanize/homework/q2/frugi/models/prices_seeds.csv'
 });
