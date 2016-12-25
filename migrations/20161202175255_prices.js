6985ea7991df9a1b1735b62264d318533e4c8643
David Shin <davidshin444@gmail.com>
2016-12-24 19:02:29 -0800

    Set up migrations and seeds

--- migrations/20161202175255_prices.js
+++ migrations/20161202175255_prices.js
@@ -2,8 +2,9 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('prices', table => {
         table.increments();
-        table.string('name', 'varchar(255)').defaultTo('').notNullable();
-        table.decimal('price_per_unit').notNullable();
+        table.string('name', 'varchar(64)').defaultTo('').notNullable();
+        table.decimal('price').notNullable();
+        table.string('unit').notNullable();
         table.integer('product_id').notNullable().references('id').inTable('products').onDelete('CASCADE');
         table.integer('retailer_id').notNullable().references('id').inTable('retailers').onDelete('CASCADE');
         table.integer('user_id').notNullable().references('id').inTable('users').onDelete('CASCADE');
