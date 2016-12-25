d331c648c555eba800e1121c5160b5701806c98a
David Shin <davidshin444@gmail.com>
2016-12-24 19:19:44 -0800

    set up the migration, seed files - time to try it

--- seeds/4_prices.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let seeder = require('knex-csv-seeder').seeder;
-console.log("seeder", seeder);
-exports.seed = seeder.seed({
-    table: 'prices',
-    file: '/Users/dave/Galvanize/homework/q2/frugi/models/prices_seeds.csv'
-});
