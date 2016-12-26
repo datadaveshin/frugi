41daa91b80d17c5ac4a5d76660c44c04e3bf47a8
David Shin <davidshin444@gmail.com>
2016-12-25 18:34:55 -0800

    Rename and clean testing basic GET on index and contacts/about

--- test/1_test-index.js
+++ test/1_test-index.js
@@ -17,39 +17,6 @@ describe("Test Chai", () => {
 });
 
 // Test GET to simply return test from a test route
-xdescribe("Test GET route to return JSON from a *former* Index page", () => {
-    it("should return 200", done => {
-        supertest(app)
-        .get('/')
-        .expect(200)
-        .end((err, res) => {
-            if (err) {return done(err)}
-            done();
-        });
-    })
-
-    it("should return json", done => {
-        supertest(app)
-        .get('/')
-        .expect('Content-type', /json/)
-        .end((err, res) => {
-            if (err) {return done(err)}
-            done();
-        });
-    });
-
-    it("should deep equal json contents", done => {
-        supertest(app)
-        .get('/')
-        .end((err, res) => {
-            if (err) {return done(err)}
-            expect(res.body).to.be.deep.equal({Babu: 'Babu'})
-            done();
-        });
-    });
-});
-
-// Test GET to simply return test from a test route
 describe("Test GET route to index", () => {
     it("should return 200", done => {
         supertest(app)
