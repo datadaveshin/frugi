f8f12c73d7e7c73792682f2ce67721241e4386a8
David Shin <davidshin444@gmail.com>
2016-12-25 18:05:21 -0800

    Clean

--- bin/csv2seed.js
+++ bin/csv2seed.js
@@ -1,9 +1,10 @@
 "use strict";
-let seeder = require('knex-csv-seeder');
-console.log(seeder);
-let seed = seeder.seeder.seed({
-  table: 'prices',
-  file: '../models/prices_seeds_2016-12-24.csv',
+//* This is an example file, put this in your seeds/ directory and use knex to run it with the command `knex seed:run --env test` */
+
+"use strict";
+let seeder = require('knex-csv-seeder').seeder;
+console.log("seeder", seeder);
+exports.seed = seeder.seed({
+    table: 'prices',
+    file: '/Users/dave/Galvanize/homework/q2/frugi/models/prices_seeds_2016-12-24_v2.csv'
 });
-seed();
-console.log(seed)
--- bin/gen_db.com
+++ bin/gen_db.com
@@ -6,5 +6,5 @@ knex seed:run --env test
 
 dropdb frugi_development --if-exists
 createdb frugi_test
-knex migrate --env test
-knex seed:run --env test
+knex migrate --env development
+knex seed:run --env development
