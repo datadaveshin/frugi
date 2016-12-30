5e66876798da7e21d2aae1eaf703cf71207d096a
David Shin <davidshin444@gmail.com>
2016-12-30 00:15:23 -0800

    Made migrations tests

--- migrations/20161202175247_retailers.js
+++ migrations/20161202175247_retailers.js
@@ -2,9 +2,9 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('retailers', table => {
         table.increments();
-        table.string('name', 'varchar(64)').defaultTo('').notNullable();
-        table.string('city', 'varchar(64)').defaultTo('').notNullable();
-        table.string('state', 'varchar(2)').defaultTo('').notNullable();
+        table.string('name', 'varchar(255)').defaultTo('').notNullable();
+        table.string('city', 'varchar(255)').defaultTo('').notNullable();
+        table.string('state', 'char(2)').defaultTo('').notNullable();
         table.timestamps(true, true);
     });
 };
--- migrations/20161202175255_prices.js
+++ migrations/20161202175255_prices.js
@@ -2,9 +2,9 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('prices', table => {
         table.increments();
-        table.string('name', 'varchar(64)').defaultTo('').notNullable();
         table.decimal('price').notNullable();
-        table.string('unit').notNullable();
+        table.decimal('amount').notNullable();
+        table.string('units').notNullable();
         table.integer('product_id').notNullable().references('id').inTable('products').onDelete('CASCADE');
         table.integer('retailer_id').notNullable().references('id').inTable('retailers').onDelete('CASCADE');
         table.integer('user_id').notNullable().references('id').inTable('users').onDelete('CASCADE');
